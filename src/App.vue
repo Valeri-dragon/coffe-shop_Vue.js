872e160678fd2958f16aaea54d63b8144b139321
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-03-23 08:49:06 +0300

    Выполнено 2 задание урока по Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-       Hello Vue.js
+  <div class="wrapper">
+    <hero-view />
   </div>
 </template>
 
 <script>
-
+import HeroView from "@/views/HeroView.vue";
 
 export default {
- 
-}
+  components: {
+    HeroView,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FooterComponent from "@/components/FooterComponent.vue";
+
+import './assets/scss/style.scss'
 
 Vue.config.productionTip = false
 
+Vue.component("footer-component", FooterComponent);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
